e5b26a863d02af23b1fa4104717a4700ad5a956a
Chinonso Okonkwo <mosesthera@gmail.com>
2025-04-01 21:05:37 +0100

    Update Cleaned_Sales_Territory_View.sql

--- Cleaned_Sales_Territory_View.sql
+++ Cleaned_Sales_Territory_View.sql
@@ -5,13 +5,13 @@ GO
 CREATE VIEW dim_sales_territory_view AS
 
 -- Cleansed dim_sales_territory Table --
-SELECT TOP 100 PERCENT [TerritoryID]
-      ,[Name]
-      ,[CountryRegionCode]
-      ,[Group]
-      ,[SalesYTD]
-      ,[SalesLastYear]
-      ,[CostYTD]
-      ,[CostLastYear]
+SELECT TerritoryID
+      ,Name
+      ,CountryRegionCode
+      ,Group
+      ,SalesYTD
+      ,SalesLastYear
+      ,CostYTD
+      ,CostLastYear
       
-FROM [AdventureWorks2019].[Sales].[SalesTerritory]
+FROM AdventureWorks2019.Sales.SalesTerritory
