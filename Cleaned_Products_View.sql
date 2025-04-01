2e0ee16a6f5a3dfa91b79771fca05781fc2e32ba
Chinonso Okonkwo <mosesthera@gmail.com>
2025-04-01 21:02:05 +0100

    Update Cleaned_Products_View.sql

--- Cleaned_Products_View.sql
+++ Cleaned_Products_View.sql
@@ -6,8 +6,7 @@ GO
 CREATE VIEW dim_products_view AS
 
 -- Cleansed DIM_Products Table --
-SELECT TOP 100 PERCENT
-  p.[ProductID] 
+SELECT p.[ProductID] 
   ,p.[Name] AS Product
   ,p.[ProductNumber] AS [Product Number]
   ,p.[MakeFlag]
@@ -28,8 +27,8 @@ SELECT TOP 100 PERCENT
   ,pc.[Name] AS Category -- Joined in from ProductCategory Table 
 	  
       
-FROM [AdventureWorks2019].[Production].[Product] as p
+FROM [AdventureWorks2019].[Production].[Product] AS p
   LEFT JOIN [AdventureWorks2019].[Production].[ProductSubcategory] AS ps ON ps.ProductSubcategoryID = p.ProductSubcategoryID
   LEFT JOIN [AdventureWorks2019].[Production].[ProductCategory] AS pc ON ps.ProductCategoryID = pc.ProductCategoryID
-ORDER by
-  p.ProductID asc
+ORDER BY
+  p.ProductID ASC
